30fa1906bfa3c14fac263f6375d53d307f4707c6
juliethquevedo <juliethquevedo158@gmail.com>
2026-05-06 15:55:24 -0500

    first commit

---
+++ Python/operaciones.py
@@ -0,0 +1,9 @@
+num1=float(input("ingrese el primer numero:"))
+num2=float(input("ingrese el segundo numero:"))
+print("suma:",num1+num2)
+print("resta:",num1-num2)
+print("multiplicacion:",num1*num2)
+if num2 != 0:
+    print("División:", num1 / num2)
+else:
+    print("Error: no se puede dividir por 0")
---
+++ Python/suma.py
@@ -0,0 +1,4 @@
+num1=int(input("Ingrese el primer numero:"))
+num2=int(input("Ingrese el segundo numero:"))
+suma=num1+num2
+print("la suma es:",suma)
