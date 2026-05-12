53b0db80b77ffe6e2786b838bffe8864ed18b32d
juliethquevedo <juliethquevedo158@gmail.com>
2026-05-12 14:43:55 -0500

    commit

---
+++ JavaScript/Ejercicio21.js
@@ -0,0 +1,13 @@
+edad=15
+if(edad>=0 && edad<=12){
+    console.log("Es niño")
+}
+else if(edad>=13 && edad<=17){
+    console.log("Es joven")
+}
+else if(edad>=18 && edad<=59){
+    console.log("Es adulto")
+}
+else{
+    console.log("Es adulto mayor")
+}
